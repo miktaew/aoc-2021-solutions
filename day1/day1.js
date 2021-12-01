3a21fbcf43f3228d1de2dc64bb46d95103471cc1
Miktaew <miloszuszycki@gmail.com>
2021-12-01 18:16:34 +0100

    day1 simplified a bit

--- day1/day1.js
+++ day1/day1.js
@@ -1,30 +1,28 @@
 const fs = require('fs');
 
 function day1() {
-    const data = fs.readFileSync('day1/input.txt', 'utf8').split("\n").map(function(item) {
-        return parseInt(item);
-    });
+    const data = fs.readFileSync('day1/input.txt', 'utf8').split("\n").map(function(item) { return parseInt(item); });
+
+    const answers = {"part 1": 0,
+                     "part 2": 0};
 
     //part 1
-    let counter = 0;
     for(let i = 1; i < data.length; i++) {
         if(data[i] > data[i-1]) {
-            counter += 1;
+            answers["part 1"] += 1;
         }
     }
 
     ////////////////////////////////////////
     //part 2
-    let counter_2 = 0;
     for(let i = 3; i < data.length; i++)
     {
         if(data[i] - data[i-3] > 0) {
-            counter_2 += 1;
+            answers["part 2"] += 1;
         }
     }
 
-    return {"part 1": counter,
-            "part 2": counter_2};
+    return answers;
 }
 
 module.exports = day1;
