bdbed785495bd3b305d4b4b1fae54f2a4750f749
Miktaew <miloszuszycki@gmail.com>
2021-12-02 13:46:07 +0100

    day2 simplified

--- day2/day2.js
+++ day2/day2.js
@@ -5,37 +5,10 @@ function day2() {
         return {direction: item.charAt(0), distance: item.slice(-1)};
     });
 
-    const answers = {"part 1": 0,
-                     "part 2": 0};
-
-    const position = {horizontal: 0, depth: 0, aim: 0};
+    const position = {horizontal: 0, aim: 0, depth: 0};
     var distance = 0;
     
-    //part 1
-    for(let i = 0; i < data.length - 1; i++) {
-
-        distance = parseInt(data[i].distance);
-
-        switch(data[i].direction) {
-            case "f":
-                position.horizontal += distance;
-                break;
-            case "d":
-                position.depth += distance;
-                break;
-            case "u":
-                position.depth -= distance;
-                break;
-        }
-    }
-
-    answers["part 1"] = position.horizontal * position.depth;
-    
-    ////////////////////////////////////////
-    //part 2
-    position.horizontal = 0;
-    position.depth = 0;
-
+    //part 1 & part 2
     for(let i = 0; i < data.length; i++) {
 
         distance = parseInt(data[i].distance);
@@ -54,9 +27,8 @@ function day2() {
         }
     }
 
-    answers["part 2"] = position.horizontal * position.depth;
-
-    return answers;
+    return {"part 1": position.horizontal * position.aim,
+            "part 2": position.horizontal * position.depth};
 }
 
 module.exports = day2;
