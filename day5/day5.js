6240991df36aa299ceb35ae7783f88fa6a43ac47
Miktaew <miloszuszycki@gmail.com>
2021-12-05 15:58:23 +0100

    day5 simplified a bit

--- day5/day5.js
+++ day5/day5.js
@@ -9,29 +9,26 @@ function day5() {
     const lines_2 = Array(1000).fill().map(() => Array(1000).fill(0));
 
     for(let i = 0; i < data.length - 1; i++) {
-        const first_x = Math.min(data[i][0][0], data[i][1][0]);
-        const second_x = Math.max(data[i][0][0], data[i][1][0]);
-        const first_y = Math.min(data[i][0][1], data[i][1][1]);
-        const second_y = Math.max(data[i][0][1], data[i][1][1]);
-        const sign_x = data[i][1][0] > data[i][0][0] ? 1 : -1; 
-        const sign_y = data[i][1][1] > data[i][0][1] ? 1 : -1;
+        const points = data[i];
 
-        if(first_x == second_x) { // X is the same
-            for(let j = first_y; j <= second_y; j++) {
-                lines_1[first_x][j] += 1;
-                lines_2[first_x][j] += 1;
+        const sign_x = points[1][0] > points[0][0] ? 1 : -1; 
+        const sign_y = points[1][1] > points[0][1] ? 1 : -1;
+
+        if(points[0][0] == points[1][0]) { // X is the same
+            for(let j = 0; j <= sign_y * (points[1][1] - points[0][1]); j++) {
+                lines_1[points[0][0]][points[0][1] + j * sign_y] += 1;
+                lines_2[points[0][0]][points[0][1] + j * sign_y] += 1;
             }
         }
-        else if(first_y == second_y) { // Y is the same
-            for(let j = first_x; j <= second_x; j++) {
-                lines_1[j][first_y] += 1;
-                lines_2[j][first_y] += 1;
+        else if(points[0][1] == points[1][1]) { // Y is the same
+            for(let j = 0; j <= sign_x * (points[1][0] - points[0][0]); j++) {
+                lines_1[points[0][0] + j * sign_x][points[0][1]] += 1;
+                lines_2[points[0][0] + j * sign_x][points[0][1]] += 1;
             }
         }
-
-        else if(first_x - second_x == first_y - second_y) { // diagonal (for part 2)
-            for(let j = 0; j <= second_x - first_x; j++) {
-                lines_2[data[i][0][0] + j * sign_x][data[i][0][1] + j * sign_y] += 1;
+        else if(sign_x*(points[0][0] - points[1][0]) == sign_y*(points[0][1] - points[1][1])) { // diagonal (for part 2)
+            for(let j = 0; j <= sign_x * (points[1][0] - points[0][0]); j++) {
+                lines_2[points[0][0] + j * sign_x][points[0][1] + j * sign_y] += 1;
             }
         }
     }
